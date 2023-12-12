f24fafa41a7d2899a88fa76c9d9b95762dab1a3c
morronepablo <morronepablo@gmail.com>
2023-12-12 01:42:59 -0300

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -70,7 +70,7 @@ export default function SignUp() {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading..." : "Sign up"}
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
